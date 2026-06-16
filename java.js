ade5bdbf5d61dbc8d023e9b9231c650d7476298b
nelly agustiani <nelyagustiani30@gmail.com>
2026-06-16 14:42:36 +0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,9 @@
+function goProfile() {
+    document.getElementById("home").classList.add("hidden");
+    document.getElementById("profil").classList.remove("hidden");
+}
+
+function goBack() {
+    document.getElementById("profil").classList.add("hidden");
+    document.getElementById("home").classList.remove("hidden");
+}
